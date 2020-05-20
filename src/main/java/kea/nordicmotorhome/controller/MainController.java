474f6157dff7df440f0bf6c0aead3e49c2faea74
Barbara Rekowska <basnow28@gmail.com>
2020-05-20 23:57:39 +0200

    Adding html files, linking styles using thymeleaf

--- src/main/java/kea/nordicmotorhome/controller/MainController.java
+++ src/main/java/kea/nordicmotorhome/controller/MainController.java
@@ -1,4 +1,28 @@
 package kea.nordicmotorhome.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+    @GetMapping("/")
+    public String dashboard(){
+        return "index.html";
+    }
+
+    @GetMapping("/createNewBooking")
+    public String createNewBooking(){
+        return "createNewBooking.html";
+    }
+
+    @GetMapping("/bookings")
+    public String bookings(){
+        return "bookings.html";
+    }
+
+    @GetMapping("/vehicles")
+    public String vehicles(){
+        return "vehicles.html";
+    }
 }
