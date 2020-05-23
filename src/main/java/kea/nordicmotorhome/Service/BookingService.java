7b97480ad98880bf1e95d089bbd62ac50f2614a1
Dagmara <przygocka.dagmara@gmail.com>
2020-05-23 20:05:58 +0200

    method for finding available vehicles in booking service

--- src/main/java/kea/nordicmotorhome/Service/BookingService.java
+++ src/main/java/kea/nordicmotorhome/Service/BookingService.java
@@ -5,9 +5,12 @@ import kea.nordicmotorhome.Model.Booking;
 
 import kea.nordicmotorhome.Model.Customer;
 
+import kea.nordicmotorhome.Model.Vehicle;
 import kea.nordicmotorhome.Repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
@@ -19,4 +22,10 @@ public class BookingService {
         return bookingRepository.createBooking(booking, customer);
     }
 
+    public List<Vehicle> findFreeVehicles(String startDate, String endDate, int vehicle_capacity){
+        return bookingRepository.findFreeVehicles(startDate, endDate, vehicle_capacity);
+    }
+    public int findSeasonRate(String startDate, String endDate){
+        return bookingRepository.findSeasonRate(startDate, endDate);
+    }
 }
