0791803155eba8e9855b689d42d3bac358fe3425
Dagmara <przygocka.dagmara@gmail.com>
2020-05-24 18:59:10 +0200

    adding new booking to database

--- src/main/java/kea/nordicmotorhome/Repository/BookingRepository.java
+++ src/main/java/kea/nordicmotorhome/Repository/BookingRepository.java
@@ -23,46 +23,69 @@ public class BookingRepository {
     public int createAddress(Customer customer){
         String sqlAddress = "INSERT INTO addresses (street_name, house_number, postcode, city, country) VALUES (?, ?, ?, ?, ?)";
         template.update(sqlAddress, customer.getStreet_name(), customer.getHouse_number(), customer.getPostcode(), customer.getCity(), customer.getCountry());
-        String sqlAddressID = "SELECT address_id FROM addresses WHERE street_name = ? AND  house_number = ? AND postcode = ? AND city = ? AND country = ?";
-        int address_id=(template.queryForObject(sqlAddressID, new Object[] {customer.getStreet_name(), customer.getHouse_number(), customer.getPostcode(), customer.getCity(), customer.getCountry()}, Integer.class));
-        return address_id;
+        String sqlAddressID = "select address_id from addresses ORDER BY address_id DESC LIMIT 1";
+        return template.queryForObject(sqlAddressID, Integer.class);
     }
-    public int createCardInfomration(Booking booking){
+    public int createCardInformation(Booking booking){
         String sqlCard = "INSERT INTO card_information (card_number, card_expiry_date, card_cvv) VALUES (?, ?, ?)";
         template.update(sqlCard, booking.getCard_number(), booking.getCard_expiry_date(), booking.getCard_cvv());
-        String sqlCardID = "SELECT card_information.card_id FROM card_information WHERE card_number = ? AND card_expiry_date = ? AND card_cvv = ?";
-        int card_id=(template.queryForObject(sqlCardID, new Object[] {booking.getCard_number(), booking.getCard_expiry_date(), booking.getCard_cvv()}, Integer.class));
-        return card_id;
+
+        String sqlCardID = "select card_information.card_id from card_information ORDER BY card_information.card_id DESC LIMIT 1";
+        return template.queryForObject(sqlCardID, Integer.class);
     }
 
-    public void createCustomer(Customer customer, int address_id){
+    public int createCustomer(Customer customer, int address_id){
         String sqlCustomer = " INSERT INTO customers (first_name, last_name, date_of_birth, phone_number, email, driver_licence_number, address_id) VALUES (?,?,?,?,?,?,?)";
         template.update(sqlCustomer, customer.getFirst_name(), customer.getLast_name(), customer.getDate_of_birth(), customer.getPhone_number(), customer.getEmail(), customer.getDriver_licence_number(), address_id);
 
-        String sqlCustomerID = "SELECT customer_id FROM customers WHERE first_name=? AND last_name=? AND date_of_birth=? AND phone_number=? AND email=? AND driver_licence_number=? AND adress_id=?";
-        customer.setCustomer_id(template.queryForObject(sqlCustomerID, new Object[] {customer.getFirst_name(), customer.getLast_name(), customer.getDate_of_birth(), customer.getPhone_number(), customer.getEmail(), customer.getDriver_licence_number(), address_id}, Integer.class));
-        System.out.println("customer"+ customer.getCustomer_id());
+        String sqlCustomerID = "select customer_id from customers ORDER BY customer_id DESC LIMIT 1";
+        int customer_id = template.queryForObject(sqlCustomerID, Integer.class);
+        customer.setCustomer_id(customer_id);
+        return customer.getCustomer_id();
+    }
+    /*public int findSeasonId(Booking booking){
+        String sqlSeason = "SELECT season_id FROM seasons WHERE season_start < ?"; //we will count whole price depending when the reservation starts, which mean that if reservation starts in mid season and ends in low season wew ill count whole price for mid season
+        int season_id = template.queryForObject(sqlSeason, new Object[] {booking.getStart_date()}, Integer.class);
+        System.out.println("Season:" + season_id);
+        return season_id;
     }
 
-    public Booking createBooking(Booking booking, Customer customer) {
+     */
+
+    public Booking createBooking(Booking booking, Customer customer) { //employee_id is manual for now
         int address_id = createAddress(customer);
-        int card_id = createCardInfomration(booking);
-        createCustomer(customer, address_id);
+        int card_id = createCardInformation(booking);
+        int customer_id = createCustomer(customer, address_id);
+        //int season_id= findSeasonId(booking);
 
-        //????????????????????
-        //String sqlSeason = "SELECT season_id FROM seasons WHERE season_start < ?"; //we will count whole price depending when the reservation starts, which mean that if reservation starts in mid season and ends in low season wew ill count whole price for mid season
-        //int season_id = template.queryForObject(sqlSeason, new Object[] {booking.getStart_date()}, Integer.class);
-
-        /*String sqlBooking = "INSERT INTO bookings (start_date, end_date, distance_driven, booking_status, payment_amount, fuel_check, booking_notes, has_picnic, has_bikerack, has_dvd_player" +
-                "has_tent, has_linen, vehicle_id, employee_id, season_id, cancellation_id, customer_id, card_id) " +
-                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
-        template.update(sqlBooking, booking.getStart_date(), booking.getEnd_date(), booking.getDistance_driven(), booking.getBooking_status(), booking.getPayment_amount(), booking.isFuel_check(), booking.getBooking_notes(), booking.isHas_picnic(), booking.isHas_bikerack(),
-                booking.isHas_dvd_player(), booking.isHas_tent(), booking.isHas_linen(), booking.getVehicle_id(), booking.getEmployee_id(), season_id, booking.getCustomer_id(), card_id);
+        String sqlBooking = "INSERT INTO bookings (" +
+                "start_date, " +
+                "end_date, " +
+                "distance_driven, " +
+                "booking_status, " +
+                "payment_amount, " +
+                "fuel_check, " +
+                "booking_notes, " +
+                "has_picnic, " +
+                "has_bikerack, " +
+                "has_dvd_player," +
+                "has_tent, " +
+                "has_linen, " +
+                "vehicle_id, " +
+                "employee_id, " +
+                "season_id, " +
+                "cancellation_id, " +
+                "customer_id, " +
+                "card_id) " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        template.update(sqlBooking, booking.getStart_date(), booking.getEnd_date(),
+                booking.getDistance_driven(), booking.getBooking_status(),
+                booking.getPayment_amount(), booking.isFuel_check(), booking.getBooking_notes(), booking.isHas_picnic(), booking.isHas_bikerack(),
+                booking.isHas_dvd_player(), booking.isHas_tent(), booking.isHas_linen(),
+                booking.getVehicle_id(), 4, 9, 9, customer_id, card_id);
 
         return booking;
 
-         */
-        return null;
     }
 
 
