3241b8e205f77171238537d5d811ce64ab871896
Barbara Rekowska <basnow28@gmail.com>
2020-05-29 14:14:53 +0200

    Adding start date and end date to Search From

--- src/main/java/kea/nordicmotorhome/Model/SearchForm.java
+++ src/main/java/kea/nordicmotorhome/Model/SearchForm.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 public class SearchForm {
     private String attribute;
     private String value;
+    private String start_date;
+    private String end_date;
 
     public String getAttribute() {
         return attribute;
@@ -21,6 +23,22 @@ public class SearchForm {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
     }
 
     @Override
