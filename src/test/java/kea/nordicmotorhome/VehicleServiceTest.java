b78efe841fd5f1484ff0a2f9cafc236a542eaa40
Dagmara <przygocka.dagmara@gmail.com>
2020-05-31 16:57:17 +0200

    bug fix for find/update vehicle

--- src/test/java/kea/nordicmotorhome/VehicleServiceTest.java
+++ src/test/java/kea/nordicmotorhome/VehicleServiceTest.java
@@ -46,7 +46,21 @@ public class VehicleServiceTest {
     //Method tests if information for specified vehicle are updated in database
     void updateVehicleTest(){
         Vehicle vehicle = new Vehicle();
-        
-
+        vehicle.setVehicle_id(1);
+        vehicle.setVehicle_brand("American Dream");
+        vehicle.setVehicle_model("42Q");
+        vehicle.setLicence_plate("L-955q");
+        vehicle.setVehicle_odometer(100000); //changed
+        vehicle.setVehicle_status("READY");
+        vehicle.setAdditional_notes(""); //changed
+        vehicle.setMechanic_status("READY"); //changed
+        vehicle.setCleaning_status("READY"); //changed
+        vehicleService.updateVehicle(vehicle);
+        assertEquals(100000, vehicleService.getVehicle(1).getVehicle_odometer());
+        assertEquals("READY", vehicleService.getVehicle(1).getMechanic_status());
+        assertEquals("READY", vehicleService.getVehicle(1).getCleaning_status());
+        assertEquals("", vehicleService.getVehicle(1).getAdditional_notes());
     }
+    
+
 }
