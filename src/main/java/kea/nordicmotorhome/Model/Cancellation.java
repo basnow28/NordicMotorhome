bdea19ce2aee7bb460d884593678c1166bcade5b
Dagmara <przygocka.dagmara@gmail.com>
2020-06-03 12:58:25 +0200

    cancellation bug

--- src/main/java/kea/nordicmotorhome/Model/Cancellation.java
+++ src/main/java/kea/nordicmotorhome/Model/Cancellation.java
@@ -9,8 +9,8 @@ import javax.persistence.Id;
 public class Cancellation {
     @Id
     private int cancellation_id;
-    private int days_range_min;
-    private int days_range_max;
+    private int days_out_min;
+    private int days_out_max;
     private double cancellation_rate;
     private int minimum_fee;
 
@@ -22,20 +22,20 @@ public class Cancellation {
         this.cancellation_id = cancellation_id;
     }
 
-    public int getDays_range_min() {
-        return days_range_min;
+    public int getDays_out_min() {
+        return days_out_min;
     }
 
-    public void setDays_range_min(int days_range_min) {
-        this.days_range_min = days_range_min;
+    public void setDays_out_min(int days_out_min) {
+        this.days_out_min = days_out_min;
     }
 
-    public int getDays_range_max() {
-        return days_range_max;
+    public int getDays_out_max() {
+        return days_out_max;
     }
 
-    public void setDays_range_max(int days_range_max) {
-        this.days_range_max = days_range_max;
+    public void setDays_out_max(int days_out_max) {
+        this.days_out_max = days_out_max;
     }
 
     public double getCancellation_rate() {
@@ -54,7 +54,7 @@ public class Cancellation {
         this.minimum_fee = minimum_fee;
     }
     public String toString(){
-        return "cancellation_id: "+cancellation_id + "cancellation_rate: "+ cancellation_rate + "days_range_min: " + days_range_min
-                 + "days_range_max: " + days_range_max;
+        return "cancellation_id: "+cancellation_id + "cancellation_rate: "+ cancellation_rate + "days_range_min: " + days_out_min
+                 + "days_range_max: " + days_out_max;
     }
 }
